5fe5127a52cc10e3faf76cf6b89b2f120c256fac
Eric Torreborre <eric.torreborre@symbiont.io>
2022-06-13 00:02:28 +0200

    feature: added repeated option values and arguments

--- src/Data/Registry/Options.hs
+++ src/Data/Registry/Options.hs
@@ -3,6 +3,8 @@
 
 module Data.Registry.Options where
 
+import Data.Registry (fun)
+import Data.Registry.Internal.Types (Typed)
 import qualified Data.Text as T
 import Protolude hiding (Option, option)
 
@@ -11,17 +13,31 @@ data CliOption a = CliOption
     _shortName :: Maybe Char,
     _metavar :: Maybe Text,
     _help :: Maybe Text,
-    _defaultValue :: Maybe a
+    _defaultValue :: Maybe a,
+    _cardinality :: Cardinality
   }
   deriving (Eq, Show)
 
 instance Semigroup (CliOption a) where
-  CliOption n1 s1 m1 h1 d1 <> CliOption n2 s2 m2 h2 d2 =
-    CliOption (n1 <|> n2) (s1 <|> s2) (m1 <|> m2) (h1 <|> h2) (d1 <|> d2)
+  CliOption n1 s1 m1 h1 d1 c1 <> CliOption n2 s2 m2 h2 d2 c2 =
+    CliOption (n1 <|> n2) (s1 <|> s2) (m1 <|> m2) (h1 <|> h2) (d1 <|> d2) (c1 <> c2)
 
 instance Monoid (CliOption a) where
-  mempty = CliOption Nothing Nothing Nothing Nothing Nothing
+  mempty = CliOption Nothing Nothing Nothing Nothing Nothing (SomeCardinality 1)
   mappend = (<>)
+
+data Cardinality
+  = SomeCardinality Int
+  | ZeroOr Int
+  | ManyCardinality
+  deriving (Eq, Show)
+
+instance Semigroup Cardinality where
+  ManyCardinality <> _ = ManyCardinality
+  __ <> ManyCardinality = ManyCardinality
+  _ <> SomeCardinality n = SomeCardinality n
+  SomeCardinality n <> ZeroOr _ = SomeCardinality n
+  ZeroOr _ <> ZeroOr n = SomeCardinality n
 
 option :: CliOption a
 option = mempty
@@ -30,30 +46,39 @@ argument :: Text -> CliOption a
 argument = metavar
 
 metavar :: Text -> CliOption a
-metavar t = CliOption Nothing Nothing (Just t) Nothing Nothing
+metavar t = option {_metavar = Just t}
 
 switch :: Char -> CliOption Bool
-switch c = CliOption Nothing (Just c) Nothing Nothing (Just True)
+switch c = option {_shortName = Just c, _defaultValue = Just True, _cardinality = SomeCardinality 0}
 
 name :: Text -> CliOption a
-name t = CliOption (Just t) Nothing Nothing Nothing Nothing
+name t = option {_name = Just t}
 
 shortName :: Char -> CliOption a
-shortName t = CliOption Nothing (Just t) Nothing Nothing Nothing
+shortName t = option {_shortName = Just t}
 
 help :: Text -> CliOption a
-help t = CliOption Nothing Nothing Nothing (Just t) Nothing
+help t = option {_help = Just t}
 
 defaultValue :: a -> CliOption a
-defaultValue a = CliOption Nothing Nothing Nothing Nothing (Just a)
+defaultValue a = option {_defaultValue = Just a}
+
+many :: CliOption a -> CliOption [a]
+many (CliOption n s m h v _) = CliOption n s m h (pure <$> v) ManyCardinality
+
+exactly :: Int -> CliOption a -> CliOption [a]
+exactly i (CliOption n s m h v _) = CliOption n s m h (pure <$> v) (SomeCardinality i)
+
+optional :: CliOption a -> CliOption (Maybe a)
+optional (CliOption n s m h v _) = CliOption n s m h (pure <$> v) ManyCardinality
 
 display :: CliOption a -> Text
-display (CliOption (Just n) Nothing (Just m) _ _) = "--" <> n <> " " <> m
-display (CliOption (Just n) (Just s) (Just m) _ _) = "[--" <> n <> "| -" <> show s <> "]" <> " " <> m
-display (CliOption (Just n) Nothing Nothing _ _) = "--" <> n
-display (CliOption (Just n) (Just s) Nothing _ _) = "--" <> n <> ", -" <> show s
-display (CliOption Nothing _ (Just m) _ _) = m
-display (CliOption Nothing _ Nothing _ _) = ""
+display (CliOption (Just n) Nothing (Just m) _ _ _) = "--" <> n <> " " <> m
+display (CliOption (Just n) (Just s) (Just m) _ _ _) = "[--" <> n <> "| -" <> show s <> "]" <> " " <> m
+display (CliOption (Just n) Nothing Nothing _ _ _) = "--" <> n
+display (CliOption (Just n) (Just s) Nothing _ _ _) = "--" <> n <> ", -" <> show s
+display (CliOption Nothing _ (Just m) _ _ _) = m
+display (CliOption Nothing _ Nothing _ _ _) = ""
 
 newtype Decoder a = Decoder {decode :: Text -> Either Text a}
 
@@ -80,82 +105,106 @@ parseCommand (Command n p) t = do
   let ts = T.strip <$> T.splitOn " " t
   case findCommandArgs n ts of
     Nothing -> Left $ "command not found: " <> n
-    Just args -> lex args >>= parseLexed p
+    Just args -> parseLexed p $ lex args
 
 findCommandArgs :: Text -> [Text] -> Maybe [Text]
 findCommandArgs _ [] = Nothing
 findCommandArgs n (n' : rest) = if n == n' then Just rest else findCommandArgs n rest
 
 parse :: Parser a -> Text -> Either Text a
-parse p = lex . fmap T.strip . T.splitOn " " >=> parseLexed p
+parse p = parseLexed p . lex . fmap T.strip . T.splitOn " "
 
-lex :: [Text] -> Either Text [Lexed]
-lex [] = Right []
-lex (n : v : rest) = do
-  case lexOptionValue n v of
-    Left _ -> do
-      case lexFlag n of
-        Right f ->
-          (f :) <$> lex (v : rest)
-        Left _ -> (:) <$> lexArg n <*> lex (v : rest)
-    Right o ->
-      (o :) <$> lex rest
-lex [n] =
-  pure <$> case lexFlag n of
-    Right v -> Right v
-    Left _ -> lexArg n
+lex :: [Text] -> [Lexed]
+lex [] = []
+lex (n : rest) = do
+  let l =
+        if isDashed n
+          then FlagName $ T.dropWhile (== '-') n
+          else ArgValue n
+  l : lex rest
 
-lexOptionValue :: Text -> Text -> Either Text Lexed
-lexOptionValue n v =
-  if T.isPrefixOf "--" n
-    then Right $ OptionValue (T.drop 2 n) v
-    else Left $ "not a correct option name, it should start with --. Got: " <> n
-
-lexFlag :: Text -> Either Text Lexed
-lexFlag n =
-  if T.isPrefixOf "-" n
-    then Right $ FlagName (T.drop 1 n)
-    else Left $ "not a correct flag name, it should start with -. Got: " <> n
-
-lexArg :: Text -> Either Text Lexed
-lexArg = Right . ArgValue
+isDashed :: Text -> Bool
+isDashed = T.isPrefixOf "-"
 
 data Lexed
   = FlagName Text
-  | OptionValue Text Text -- optionName, optionValue
-  | ArgValue Text -- optionName, optionValue
+  | ArgValue Text
   deriving (Eq, Show)
 
-parser :: CliOption a -> Decoder a -> Parser a
-parser o d = Parser $ \lexed ->
-  case findOption o lexed of
-    Just (OptionValue _ v) ->
-      decode d v
-    Just (FlagName _) ->
-      case _defaultValue o of
-        Nothing -> Left $ "missing default value for " <> display o
-        Just v -> pure v
-    Just (ArgValue v) ->
-      decode d v
-    Nothing ->
-      case _defaultValue o of
-        Nothing ->
-          Left $ "missing option value for " <> display o
-        Just v ->
-          pure v
+isArgValue :: Lexed -> Bool
+isArgValue (ArgValue _) = True
+isArgValue _ = False
 
-findOption :: CliOption a -> [Lexed] -> Maybe Lexed
-findOption _o [] = Nothing
-findOption o (f@(FlagName n) : rest) =
-  if (T.singleton <$>_shortName o) == Just n
-    then Just f
-    else findOption o rest
-findOption o (ov@(OptionValue n _) : rest) =
-  if _name o == Just n
-    then Just ov
-    else findOption o rest
-findOption _o (ArgValue v : _) =
-  Just (ArgValue v)
+lexedValue :: Lexed -> Maybe Text
+lexedValue (ArgValue t) = Just t
+lexedValue _ = Nothing
+
+unlexValues :: [Lexed] -> Text
+unlexValues = T.intercalate " " . mapMaybe lexedValue
+
+parser :: forall a. (Typeable a) => CliOption a -> Typed (Decoder a -> Parser a)
+parser o = fun $ \d ->
+  Parser $ \lexed -> do
+    case getName o of
+      Just n ->
+        case findOption o n lexed of
+          [] -> case _defaultValue o of
+            Nothing -> Left $ "missing default value for flag: " <> display o
+            Just def -> Right def
+          ls -> decode d (unlexValues ls)
+      Nothing -> do
+        let args = takeWhile isArgValue $ lexed
+        case _cardinality o of
+          SomeCardinality i ->
+            decode d (unlexValues $ take i args)
+          ZeroOr i ->
+            case args of
+              [] ->
+                case _defaultValue o of
+                  Just def -> pure def
+                  Nothing -> Left $ "missing value for argument " <> display o
+              as
+                | length as == i ->
+                  decode d (unlexValues as)
+              _ ->
+                Left $ "expected 0 or " <> show i <> " arguments. Got: " <> unlexValues args
+          ManyCardinality ->
+            decode d (unlexValues args)
+
+data Name
+  = LongShort Text Text
+  | LongOnly Text
+  | ShortOnly Text
+  deriving (Eq, Show)
+
+findOption :: CliOption a -> Name -> [Lexed] -> [Lexed]
+findOption _ _ [] = []
+findOption o n ls = do
+  let args = drop 1 $ dropWhile (not . sameName n) ls
+  case _cardinality o of
+    SomeCardinality i ->
+      take i args
+    ZeroOr i ->
+      case args of
+        [] -> []
+        vs | length vs == i -> vs
+        _ -> []
+    ManyCardinality ->
+      args
+
+sameName :: Name -> Lexed -> Bool
+sameName (LongShort n s) (FlagName f) = n == f || s == f
+sameName (LongOnly n) (FlagName f) = n == f
+sameName (ShortOnly n) (FlagName f) = n == f
+sameName _ _ = False
+
+getName :: CliOption a -> Maybe Name
+getName o =
+  case (_name o, _shortName o) of
+    (Just n, Just s) -> Just $ LongShort n $ T.singleton s
+    (Just n, Nothing) -> Just $ LongOnly n
+    (Nothing, Just s) -> Just $ ShortOnly $ T.singleton s
+    (Nothing, Nothing) -> Nothing
 
 -- * DECODERS
 
@@ -166,4 +215,7 @@ boolDecoder :: Decoder Bool
 boolDecoder = Decoder $ \t -> maybe (Left $ "cannot read as a Bool: " <> t) Right (readMaybe t)
 
 textDecoder :: Decoder Text
-textDecoder = Decoder $ \t -> Right t
+textDecoder = Decoder Right
+
+manyOf :: Decoder a -> Decoder [a]
+manyOf d = Decoder $ \t -> for (T.strip <$> T.splitOn " " t) (decode d)
--- test/Test/Data/Registry/Options/HelloSpec.hs
+++ test/Test/Data/Registry/Options/HelloSpec.hs
@@ -2,47 +2,47 @@ module Test.Data.Registry.Options.HelloSpec where
 
 import Data.Registry
 import Data.Registry.Options
-import Protolude hiding (Option, option)
+import Protolude hiding (Option, many, option)
 import Test.Tasty.Hedgehogx hiding (defaultValue)
 
 test_lexed = test "lex the command line" $ do
-  lexFlag "-o" === Right (FlagName "o")
-  lexOptionValue "--o" "v" === Right (OptionValue "o" "v")
-  lex ["-o", "--o", "v"] === Right [FlagName "o", OptionValue "o" "v"]
-  lex ["-q", "--hello", "eric", "--repeat", "10"] === Right [FlagName "q", OptionValue "hello" "eric", OptionValue "repeat" "10"]
-  lex ["-q", "eric", "--repeat", "10"] === Right [FlagName "q", ArgValue "eric", OptionValue "repeat" "10"]
+  lex ["-o", "--o", "v"] === [FlagName "o", FlagName "o", ArgValue "v"]
+  lex ["-q", "--hello", "eric", "--repeat", "10"] === [FlagName "q", FlagName "hello", ArgValue "eric", FlagName "repeat", ArgValue "10"]
+  lex ["-q", "eric", "--repeat", "10"] === [FlagName "q", ArgValue "eric", FlagName "repeat", ArgValue "10"]
+  lex ["-q", "eric", "etorreborre", "--repeat", "10"] === [FlagName "q", ArgValue "eric", ArgValue "etorreborre", FlagName "repeat", ArgValue "10"]
+  lex ["-q","--repeat", "10", "eric", "etorreborre"] === [FlagName "q", FlagName "repeat", ArgValue "10", ArgValue "eric", ArgValue "etorreborre"]
 
 test_hello_parser = test "simple hello parser" $ do
-  let parsers =
-        funTo @Parser Sample
-          <: fun (parser @Bool)
-          <: fun (parser @Int)
-          <: fun (parser @Text)
-          <: val repeatOption
-          <: val quietFlag
-          <: val helloOption
-          <: fun intDecoder
-          <: fun boolDecoder
-          <: fun textDecoder
-
   let p = make @(Parser Sample) parsers
   parse p "-q --hello eric --repeat 10" === Right (Sample "eric" True 10)
 
 test_parse_argument = test "parse options and arguments" $ do
-  let parsers =
-        funTo @Parser Sample
-          <: fun (parser @Bool)
-          <: fun (parser @Int)
-          <: fun (parser @Text)
-          <: val repeatOption
-          <: val quietFlag
-          <: val helloArgument
-          <: fun intDecoder
-          <: fun boolDecoder
-          <: fun textDecoder
+  let p = make @(Parser Sample) (parser (argument @Text "hello") <: parsers)
+  parse p "eric -q --repeat 10" === Right (Sample "eric" True 10)
 
-  let p = make @(Parser Sample) parsers
-  parse p "-q --repeat 10 eric" === Right (Sample "eric" True 10)
+test_parse_many_arguments = test "parse options and arguments with repeated values" $ do
+  let parsers' =
+        funTo @Parser SampleRepeated
+          <: parser (many (argument @Text "hello"))
+          <: parser (many (name @Int "repeat"))
+          <: parser (switch 'q')
+          <: parsers
+
+  let p = make @(Parser SampleRepeated) parsers'
+  parse p "eric etorreborre -q --repeat 10 12" === Right (SampleRepeated ["eric", "etorreborre"] True [10, 12])
+
+-- * HELPERS
+
+parsers =
+  funTo @Parser Sample
+    <: parser (name @Text "hello")
+    <: parser (switch 'q')
+    <: parser (name @Int "repeat")
+    <: fun (manyOf @Int)
+    <: fun (manyOf @Text)
+    <: fun intDecoder
+    <: fun boolDecoder
+    <: fun textDecoder
 
 data Sample = Sample
   { hello :: Text,
@@ -51,14 +51,9 @@ data Sample = Sample
   }
   deriving (Eq, Show)
 
-repeatOption :: CliOption Int
-repeatOption = name "repeat"
-
-quietFlag :: CliOption Bool
-quietFlag = switch 'q'
-
-helloOption :: CliOption Text
-helloOption = name "hello"
-
-helloArgument :: CliOption Text
-helloArgument = argument "hello"
+data SampleRepeated = SampleRepeated
+  { hello1 :: [Text],
+    quiet1 :: Bool,
+    repeat1 :: [Int]
+  }
+  deriving (Eq, Show)
