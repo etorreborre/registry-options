d3ce9b99397caf901a52a41b351a01b2417de10d
Eric Torreborre <eric.torreborre@symbiont.io>
2022-07-02 00:22:16 +0200

    doc: more comments

--- src/Data/Registry/Options/CliParsers.hs
+++ src/Data/Registry/Options/CliParsers.hs
@@ -13,6 +13,12 @@ import Data.Registry.Options.Lexed
 import GHC.TypeLits
 import Protolude hiding (option)
 
+-- | Create an option:
+--     - with a short/long name
+--     - a metavar
+--     - no active/default values
+--
+--   The [CliOption] list can be used to override values or provide a help
 option :: forall s a. (KnownSymbol s, Typeable a, Show a) => [CliOption] -> Registry _ _
 option os = do
   let fieldType = showType @a
@@ -20,6 +26,13 @@ option os = do
     <+ noDefaultValue @s @a
     <+ noActiveValue @s @a
 
+-- | Create a flag:
+--     - with a short/long name
+--     - a metavar
+--     - an active value
+--     - an optional default value
+--
+--   The [CliOption] list can be used to override values or provide a help
 flag :: forall s a. (KnownSymbol s, Typeable a, Show a) => a -> Maybe a -> [CliOption] -> Registry _ _
 flag activeValue defaultValue os = do
   let fieldType = showType @a
@@ -27,6 +40,13 @@ flag activeValue defaultValue os = do
     <+ maybe (noDefaultValue @s @a) (setDefaultValue @s @a) defaultValue
     <+ setActiveValue @s @a activeValue
 
+-- | Create a switch:
+--     - with a short/long name
+--     - a metavar
+--     - an active value: True
+--     - an default value: False
+--
+--   The [CliOption] list can be used to override values or provide a help
 switch :: forall s. (KnownSymbol s) => [CliOption] -> Registry _ _
 switch os = do
   let fieldType = showType @Bool
@@ -34,6 +54,14 @@ switch os = do
     <+ setDefaultValue @s False
     <+ setActiveValue @s True
 
+-- | Create an argument:
+--     - with no short/long names
+--     - a metavar
+--     - no active/default values
+--
+--   The [CliOption] list can be used to override values or provide a help
+--
+--   When the argument is read, its value is removed from the list of lexed values
 argument :: forall s a. (KnownSymbol s, Typeable a, Show a) => [CliOption] -> Registry _ _
 argument os = do
   let fieldType = showType @a
@@ -41,6 +69,14 @@ argument os = do
     <+ noDefaultValue @s @a
     <+ noActiveValue @s @a
 
+-- | Create a positional argument, to parse the nth value (starting from 0):
+--     - with no short/long names
+--     - a metavar
+--     - no active/default values
+--
+--   The [CliOption] list can be used to override values or provide a help
+--
+--   When the argument is read, its value is left in the list of lexed values
 positional :: forall s a. (KnownSymbol s, Typeable a, Show a) => Int -> [CliOption] -> Registry _ _
 positional n os = do
   let p fieldOptions = \dv av d -> Parser @s @a $ \ls -> do
@@ -56,14 +92,24 @@ positional n os = do
     <+ noDefaultValue @s @a
     <+ noActiveValue @s @a
 
+-- | Create an anonymous argument:
+--     - with no short/long names
+--     - a metavar
+--     - no active/default values
+--
+--   The [CliOption] list can be used to override values or provide a help
+--
+--   When the argument is read, its value is removed from the list of lexed values
 anonymous :: forall a. (Typeable a, Show a) => [CliOption] -> Registry _ _
 anonymous os =
   fun (\fieldOptions -> parseField @Anonymous @a fieldOptions Nothing (showType @a) os)
     <+ (noDefaultValue @Anonymous @a)
     <+ (noActiveValue @Anonymous @a)
 
+-- | Set an active value for a given field name and field type
 setActiveValue :: forall s a. (KnownSymbol s, Typeable a) => a -> Typed (ActiveValue s a)
 setActiveValue = createActiveValue @s @a . toDyn
 
+-- | Set a default value for a given field name and field type
 setDefaultValue :: forall s a. (KnownSymbol s, Typeable a) => a -> Typed (DefaultValue s a)
 setDefaultValue = createDefaultValue @s @a . toDyn
